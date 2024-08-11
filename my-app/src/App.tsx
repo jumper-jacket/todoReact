90c24426b77ebe314938acef238a84c61209a3b3
User <u96464224@gmail.com>
2024-08-12 06:58:34 +0900

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1 className="text-4xl font-bold">
+        tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;

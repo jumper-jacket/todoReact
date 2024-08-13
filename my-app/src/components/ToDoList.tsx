382783518833e425bd8c4117c601cb4a05a1fb22
User <u96464224@gmail.com>
2024-08-13 17:40:12 +0900

    added functionality to the app.

--- my-app/src/components/ToDoList.tsx
+++ my-app/src/components/ToDoList.tsx
@@ -37,9 +37,10 @@ function ToDoList(){
         setTodos(todos.map(todo => 
             todo.id === id ? {...todo, completed: !todo.completed} : todo
         ));
+        console.log(todos);
     }
-   
-    return (
+
+        return (
     <div className="mx-auto mt-10 rounded-lg">
             <h1 className="text-3xl font-bold mb-4">ToDoList</h1>
             <div className="flex mb-4">
@@ -56,7 +57,7 @@ function ToDoList(){
             >Add</button>
         </div>
             <ul>
-                {todos.map((todo,index) => (
+                {todos.filter(todo => todo.completed===false).map(todo => (
                     <li
                     key={todo.id}
                     className={`p-2 mb-2 rounded flex justify-between items-center ${todo.completed ? 'bg-green-200' : 'bg-gray-200'}`}
@@ -82,6 +83,37 @@ function ToDoList(){
                     </li>
                 ))}
             </ul>
+            <h1 className="text-4xl">Completed</h1>
+            <ul>
+                {todos.filter(todo => todo.completed===true)
+                .map(todo => (
+                    <li
+                    key={todo.id}
+                    className={`p-2 mb-2 rounded flex justify-between items-center ${todo.completed ? 'bg-green-200' : 'bg-gray-200'}`}
+                    >
+                        <div>
+                            <input
+                            type="checkbox"
+                            checked={!todo.completed}
+                            onChange={()=> toggleComplete(todo.id)}
+                            className='mr-2'
+                            />
+                            <span className={todo.completed ? 'line-through' : ''}>
+                                {todo.text}
+                            </span>
+                            <span className="text-sm text-gray-500 ml-2">
+                                登録日:{todo.registeredTime}
+                            </span>
+                        </div>
+                        <button
+                        onClick={()=> deleteTodo(todo.id)}
+                        className='bg-red-500 text-white rounded p-1 text-sm'
+                        >削除</button>
+                    </li>
+
+                ))}
+            </ul>
+
     </div>
     )
 }
